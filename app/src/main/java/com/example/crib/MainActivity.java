1e72b6f467c97fd61cb019890b5504f1883749cc
Anastasiia <nastya589sht@gmail.com>
2022-05-24 10:31:16 +0300

    Crib

---
+++ app/src/main/java/com/example/crib/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.crib;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String titles[] = {
+            "1. История основания",
+            "2. Альбомы",
+            "3. Популярные клипы",
+            "4. Дэн Плацман",
+            "5. Дэн Рейнольдс",
+            "6. Бен МакКи",
+            "7. Уэйн Сермон",
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        listView.setAdapter(
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles));
+        listView.setTextFilterEnabled(true);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, DetailActivity.class);
+
+                intent.putExtra("title", position);
+
+
+                startActivity(intent);
+            }
+        });
+    }
+}
